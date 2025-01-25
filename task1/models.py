4bafe1a19607194e159380ddf75f3bf4f01ed6c1
Vladickush <u.vn@mail.ru>
2025-01-25 20:52:02 +0300

    first commit

---
+++ task1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=100, unique=True) # имя покупателя
+    balance = models.DecimalField(max_digits=10, decimal_places=2)  # баланс покупателя
+    age = models.IntegerField(default=18)  # возраст
+
+class Game(models.Model):
+    title = models.CharField(max_length=100, unique=True) # название игры
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # цена
+    size = models.DecimalField(max_digits=10, decimal_places=2)  # размер файлов игры
+    description = models.TextField(blank=True)  # Описание игры
+    age_limited = models.BooleanField(default=False)    # ограничение возраста 18+
+    buyers = models.ManyToManyField(Buyer)  # Связь "многие ко многим"
