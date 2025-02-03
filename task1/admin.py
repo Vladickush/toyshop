5b96cd256472f00d10d0cf091ee5f275ce2fdf5e
Vladickush <u.vn@mail.ru>
2025-02-03 22:51:40 +0300

    Update admin.py

--- task1/admin.py
+++ task1/admin.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
-from .models import Game, Buyer
+from .models import Game, Buyer, News
+
 
 # Register your models here.
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_filter = ('size', 'cost',)             #Фильтрацию по полям size и cost.
-    list_display = ('title', 'cost', 'size',)   #Отображение title, cost и size при отображении всех полей списком.
-    search_fields = ('title',)                  #Поиск по полю title.
-    list_per_page = 20                          #Ограничение кол-ва записей до 20.
+    list_filter = ('size', 'cost',)  # Фильтрацию по полям size и cost.
+    list_display = ('title', 'cost', 'size',)  # Отображение title, cost и size при отображении всех полей списком.
+    search_fields = ('title',)  # Поиск по полю title.
+    list_per_page = 20  # Ограничение кол-ва записей до 20.
+
 
 @admin.register(Buyer)
 class BuyerAdmin(admin.ModelAdmin):
-    list_filter = ('balance', 'age',)          #Фильтрацию по полям balance и age.
-    list_display = ('name', 'balance', 'age',) #Отображение name, balance и age при отображении всех полей списком.
-    search_fields = ('name',)                  #Поиск по полю name.
-    list_per_page = 30                         #Ограничение кол-ва записей до 30.
-    readonly_fields = ('balance',)             #Доступным только для чтения поле balance.
+    list_filter = ('balance', 'age',)  # Фильтрацию по полям balance и age.
+    list_display = ('name', 'balance', 'age',)  # Отображение name, balance и age при отображении всех полей списком.
+    search_fields = ('name',)  # Поиск по полю name.
+    list_per_page = 30  # Ограничение кол-ва записей до 30.
+    readonly_fields = ('balance',)  # Доступным только для чтения поле balance.
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'date')
 
